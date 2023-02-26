7e2df96efaa76e2af6735d8630bba8711930b177
Marlon Marín <marlon.1701910281@ucaldas.edu.co>
2023-02-26 13:58:21 -0500

    Creación de datasource con mongodb y cambio de puerto en el servidor por el 3001

--- src/datasources/mongodb.datasource.ts
+++ src/datasources/mongodb.datasource.ts
@@ -13,7 +13,6 @@ const config = {
   useNewUrlParser: true
 };
 
-// Puse el nombre mal
 // Observe application's life cycle to disconnect the datasource when
 // application is stopped. This allows the application to be shut down
 // gracefully. The `stop()` method is inherited from `juggler.DataSource`.
