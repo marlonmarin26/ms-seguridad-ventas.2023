7910a9e5966ac1ce40cbe8e39c198e591ef80a1b
Marlon Marín <marlon.1701910281@ucaldas.edu.co>
2023-03-15 12:31:56 -0500

    Creación de usuario con clave aleatoria cifrada

--- src/models/usuario.model.ts
+++ src/models/usuario.model.ts
@@ -47,9 +47,8 @@ export class Usuario extends Entity {
 
   @property({
     type: 'string',
-    required: true,
   })
-  clave: string;
+  clave?: string;
 
   @hasMany(() => Login)
   logins: Login[];
